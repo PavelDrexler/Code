9a237574d7d7f75df0f2459b37f8ec210b47f0bb
Pavel Drexler <pavel.drexler87@gmail.com>
2019-11-19 18:39:48 +0100

    Changed piskvorky - defined more functions and changed game so it uses them.

--- Projects/piskvorky.py
+++ Projects/piskvorky.py
@@ -10,9 +10,9 @@ field = ["-", "-", "-",
          "-", "-", "-"]
 separator = 30*"="
 
-def game():
+def piskvorky():
 	gameContinue = 1
-	winner = 0
+	playerWin = 0
 	player = "X"
 	print (separator)
 	print("Welcome to Tic-Tac-Toe, best game of the year!")
@@ -27,10 +27,9 @@ def game():
 		print("\n")
 
 
-	while gameContinue:
-		#repeat game
-		displayField()
+	
 
+	def getInput():		
 		print(player+ "'s turn.")
 		position =input ("Select position from 1-9: ")
 
@@ -56,76 +55,93 @@ def game():
 				valid = True
 			else:
 				print("You can't go there. Go again.")
+		return position
+
+
+
+
 		
 
-
-		field[int(position)] = player
-		print (separator)
-
-		#check if winner:
-
+	#check if there is a winner:
+	def checkWinner(fieldInput):
 		#row
-		row_1 = field[0] == field[1] == field[2] != "-"
-		row_2 = field[3] == field[4] == field[5] != "-"
-		row_3 = field[6] == field[7] == field[8] != "-"
+		row_1 = fieldInput[0] == fieldInput[1] == fieldInput[2] != "-"
+		row_2 = fieldInput[3] == fieldInput[4] == fieldInput[5] != "-"
+		row_3 = fieldInput[6] == fieldInput[7] == fieldInput[8] != "-"
 			
 		if row_1: 
-			winner = field[0]
+			winner = fieldInput[0]
 			gameContinue = 0
 		elif row_2: 
-			winner = field[3]
+			winner = fieldInput[3]
 			gameContinue = 0
 		elif row_3: 
-			winner = field[6]
+			winner = fieldInput[6]
 			gameContinue = 0		
 		#column
-		col_1 = field[0] == field[3] == field[6] != "-"
-		col_2 = field[1] == field[4] == field[7] != "-"
-		col_3 = field[2] == field[5] == field[8] != "-"
+		col_1 = fieldInput[0] == fieldInput[3] == fieldInput[6] != "-"
+		col_2 = fieldInput[1] == fieldInput[4] == fieldInput[7] != "-"
+		col_3 = fieldInput[2] == fieldInput[5] == fieldInput[8] != "-"
 
 		if col_1: 
-			winner = field[0]
+			winner = fieldInput[0]
 			gameContinue = 0
 		elif col_2: 
-			winner = field[1]
+			winner = fieldInput[1]
 			gameContinue = 0
 		elif col_3: 
-			winner = field[2]
+			winner = fieldInput[2]
 			gameContinue = 0
 
 		#diagonal
-		diag_1 = field[0] == field[4] == field[8] != "-"
-		diag_2 = field[2] == field[4] == field[6] != "-"
+		diag_1 = fieldInput[0] == fieldInput[4] == fieldInput[8] != "-"
+		diag_2 = fieldInput[2] == fieldInput[4] == fieldInput[6] != "-"
 		if diag_1: 
-			winner = field[0]
+			winner = fieldInput[0]
 			gameContinue = 0
 		elif diag_2: 
-			winner = field[2]
+			winner = fieldInput[2]
 			gameContinue = 0
 
 		#Deuce
-		elif "-" not in field:
+		elif "-" not in fieldInput:
 			winner = "Developer - you guys sux!"
 			gameContinue = 0
+		else:
+			winner = 0
+
+		return winner
 
 
 
-		#Flip player
-		if player == "X":
-			player = "O"
+	
+	#Flip player
+	def flipPlayer(playerInput):
+		if playerInput == "X":
+			playerInput = "O"
   
-		elif player == "O":
-			player = "X"
+		elif playerInput == "O":
+			playerInput = "X"
+		return playerInput
+
+
+	#Function of the game
+	while gameContinue:
+		#repeat game
+		displayField()
+		
+		field[int(getInput())] = player
+		print (separator)
+		playerWin = checkWinner(field)
+		if playerWin != 0 :
+			print("Final state:")
+			displayField()
+			print (separator)
+			return playerWin
+		player = flipPlayer(player)
 
 
 		
 
-
-		
-	print("Final state:")
-	displayField()
-	print (separator)
-	return winner
-
-victor = game()
+victor = piskvorky()
 print("Victory goes to ", victor)
